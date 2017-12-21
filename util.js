b37d94b7e833119ea1dfbce4fa935ae54a47f090
beim <beim2015@outlook.com>
2017-12-21 08:20:19 +0000

    1

--- util.js
+++ util.js
@@ -2,10 +2,11 @@ const fs = require('fs')
 const path = require('path')
 
 const genLabels = () => {
-    IMG_DIR = 'caffesource/rawimages/images'
+    IMG_DIR = 'caffesource/rawimgs/images'
     names = fs.readdirSync(path.join(__dirname, IMG_DIR))
     console.log(names)
 }
+genLabels()
 
 const convImgs2Lmdb = () => {
 
