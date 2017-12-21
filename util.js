b21fab105bcd0f65ca6a2bf2010603196627e583
brianxkliu(刘星焜) <brianxkliu@tencent.com>
2017-12-21 16:18:56 +0800

    '1'

--- util.js
+++ util.js
@@ -1,6 +1,10 @@
+const fs = require('fs')
+const path = require('path')
 
 const genLabels = () => {
-
+    IMG_DIR = 'caffesource/rawimages/images'
+    names = fs.readdirSync(path.join(__dirname, IMG_DIR))
+    console.log(names)
 }
 
 const convImgs2Lmdb = () => {
