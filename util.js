d4908c3d60e055f5875a22583c29ec6adb847db8
brianxkliu(刘星焜) <brianxkliu@tencent.com>
2017-12-21 17:08:00 +0800

    '1'

--- util.js
+++ util.js
@@ -1,21 +1,22 @@
 const fs = require('fs')
 const path = require('path')
+const child_process = require('child_process')
+
+const ROOT_DIR = path.join(__dirname, 'caffesource')
+const IMG_PATH = path.join(ROOT_DIR, 'rawimgs/images')
+const TRAIN_TXT_PATH = path.join(ROOT_DIR, 'rawimgs/train.txt')
+const TEST_TXT_PATH = path.join(ROOT_DIR, 'rawimgs/val.txt')
+const LMDB_DIR = path.join(ROOT_DIR, 'lmdbimgs')
 
 const genLabels = () => {
-    ROOT_DIR = path.join(__dirname, 'caffesource/rawimgs')
-    IMG_PATH = path.join(ROOT_DIR, 'images')
-    TRAIN_TXT_PATH = path.join(ROOT_DIR, 'train.txt')
-    TEST_TXT_PATH = path.join(ROOT_DIR, 'val.txt')
-
-    label_names = fs.readdirSync(IMG_PATH)
-    train_txt_fd = fs.openSync(TRAIN_TXT_PATH, 'w')
-    test_txt_fd = fs.openSync(TEST_TXT_PATH, 'w')
+    const label_names = fs.readdirSync(IMG_PATH)
+    const train_txt_fd = fs.openSync(TRAIN_TXT_PATH, 'w')
+    const test_txt_fd = fs.openSync(TEST_TXT_PATH, 'w')
     for (let label of label_names) {
-        img_names = fs.readdirSync(path.join(IMG_PATH, label))
+        const img_names = fs.readdirSync(path.join(IMG_PATH, label))
         for (let j in img_names) {
-            img = img_names[j]
-            img_path = path.join(IMG_PATH, label, img)
-            p = path.join(IMG_PATH, label, img) + ` ${label}\n`
+            const img = img_names[j]
+            const p = path.join(IMG_PATH, label, img) + ` ${label}\n`
             if (j % 5 !== 0) {
                 fs.writeSync(train_txt_fd, p)
             }
@@ -27,8 +28,9 @@ const genLabels = () => {
     fs.closeSync(train_txt_fd)
     fs.closeSync(test_txt_fd)
 }
-genLabels()
 
 const convImgs2Lmdb = () => {
-
+    const CONVERT_TOOL = path.join(ROOT_DIR, 'rawimgs/create_imgs_lmdb.sh')
+    let stdout = child_process.execSync(`"${CONVERT_TOOL}" ${TRAIN_TXT_PATH} ${TEST_TXT_PATH} ${LMDB_DIR}`)
+    console.log(stdout)
 }
